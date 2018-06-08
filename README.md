c84d07c54b271b479b1e1fd29146d58fbf07533d
taylor-josh <jtaylor9110@gmail.com>
2018-06-07 22:45:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### :book: JS RSS Reader ###
+
+A simple, barebones RSS reader written in ES6 JavaScript.
+
+No accounts, no social media integration... or really much of anything.
+
+---
+
+In an effort to learn more about JS, especially ES6, I wanted to create something I can use - I end up more invested in building things for either myself or others vs foo bar baz tutorials.
+
+Being as how most RSS readers these days require an account, I wanted to build an RSS reader that I can use on my local machine, with no account, and still check out all the sites I like reading in one place.
+
+NOTE: This is a personal project where I am building something for myself to use so code will likely be a little messy as I learn more and add features to this project as I think of them. If you like what you see, feel free to fork/clone. I don't anticipate anyone submitting PRs but you can do that as well if you feel so inclined and I'll check them out/merge them.
---
+++ src/index.js
@@ -0,0 +1,113 @@
+const feedItems = [];
+const feedElement = document.querySelector(".feed");
+const feedFile = "src/feedURLs.json";
+
+function logResult(res) {
+  return res;
+}
+
+function logError(err) {
+  console.log(`Uh oh, looks like there was a problem : \n ${err} `);
+}
+
+function validateResponse(res) {
+  if (!res.ok) {
+    throw Error(res.text);
+  }
+  return res;
+}
+
+function readResponseAsText(res) {
+  return res.text();
+}
+
+function parseFieldsFromXml(xmlDoc) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(xmlDoc, "text/xml");
+
+  const feedTitle = doc.querySelector("channel title").textContent;
+
+  const items = doc.querySelectorAll("item").forEach(item => {
+    const itemTitle = item.querySelector("title").textContent;
+    const itemDesc = item.querySelector("description").textContent;
+    const itemLink = item.querySelector("link").textContent;
+
+    feedItems.push({
+      feedTitle: feedTitle,
+      itemTitle: itemTitle,
+      itemLink: itemLink,
+      itemDescr: itemDesc
+    });
+  });
+
+  renderHTML(feedItems);
+}
+
+function getUniqueArrayValues(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function generateArticleMarkup(articles) {
+  return `
+  <ul>
+      ${articles.map(
+        article =>
+          `<li> <a href="${article.itemLink}"> ${article.itemTitle} </a> </li>`
+      )}
+  </ul>
+  `;
+}
+
+function renderHTML(feedArticles) {
+  let articlesPerSite = [];
+
+  const uniqueTitles = feedArticles
+    .map(feed => feed.feedTitle)
+    .filter(getUniqueArrayValues);
+
+  // generate markup for the articles with a unique header per site
+  for (let i = 0; i < uniqueTitles.length; i++) {
+    let articlesPerTitle = feedArticles.filter(article => {
+      return article.feedTitle == uniqueTitles[i];
+    });
+
+    const feedMarkup = `
+      <h4> ${uniqueTitles[i]} </h4>
+      ${generateArticleMarkup(articlesPerTitle)
+        .split(",")
+        .join("")}
+    `;
+
+    articlesPerSite.push(feedMarkup);
+  }
+
+  feedElement.innerHTML = articlesPerSite.join("");
+}
+
+// use the feedURL attribute for the objects in the JSON
+// file and fetch/return the XML for each site
+function fetchXmlForFeeds(resourceJSON) {
+  const jsonObj = JSON.parse(resourceJSON);
+
+  Object.keys(jsonObj).forEach(key => {
+    // TODO: See if there is a way to not have to rely on cors-anywhere
+    // or any other third party service while testing locally
+    fetch(jsonObj[key].feedUrl)
+      .then(validateResponse)
+      .then(readResponseAsText)
+      .then(parseFieldsFromXml)
+      .then(logResult)
+      .catch(logError);
+  });
+}
+
+function fetchFeedFile(jsonFile) {
+  fetch(jsonFile)
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(fetchXmlForFeeds)
+    .then(logResult)
+    .catch(logError);
+}
+
+fetchFeedFile(feedFile);
